5de237cf06e8a35f679c2814854c4ed892c0b8d1
SUDIKSHACHANDEL <Sudikshachandel15112@gmail.com>
2017-07-08 16:02:27 +0530

    Access Token and Base Url

---
+++ constants.py
@@ -0,0 +1,9 @@
+#all global variable is here
+
+#instagram api access token
+
+APP_ACCESS_TOKEN='4624358886.3ea7866.071ce90e82fa44b38b4d372ca023a40e'
+
+#instagram BASE URL
+
+BASE_URL = 'https://api.instagram.com/v1/'
