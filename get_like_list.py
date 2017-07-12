e7b32e4265f0d27601868bc2779317849ad1d371
SUDIKSHACHANDEL <Sudikshachandel15112@gmail.com>
2017-07-12 14:59:30 +0530

    recreate like list

--- get_like_list.py
+++ get_like_list.py
@@ -1,7 +1,7 @@
 import requests
 from constants import BASE_URL, APP_ACCESS_TOKEN
 from get_post_id import get_post_id
-insta_username = 'shramatanu9878'
+
 def like_list(insta_username):
   media_id = get_post_id(insta_username)
 
@@ -10,7 +10,7 @@ def like_list(insta_username):
   get_a_like = requests.get(request_url).json()
   print get_a_like
   if get_a_like['meta']['code'] == 200:
-    print("hgghgh")
+
     print"Media with media id '{}' is liked by following users:" . format(media_id)
     for (index,user_likes) in enumerate(get_a_like['data']):
         print"{}. {} ({}) - {}".format(index+1,user_likes['full_name'],user_likes['id'],user_likes['username'])
@@ -20,4 +20,3 @@ def like_list(insta_username):
     print("There is no Like regarding this post.")
 
 
-like_list(insta_username = 'sharmatanu9878')
